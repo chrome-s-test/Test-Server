ac1207c9f27a451e67efe12fa456174eba024028
gmlstjq123 <gmlstjq123@naver.com>
2023-12-25 16:15:16 +0900

    update submodule

--- src/main/java/config/test/TestApplication.java
+++ src/main/java/config/test/TestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
+	// 파일을 임의로 수정합니다.
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
